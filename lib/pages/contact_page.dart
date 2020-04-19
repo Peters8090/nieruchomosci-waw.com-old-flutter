2a692a1d199ae13bf1434c622ecc3d4f1f3fc63b
Peters8090 <52279875+Peters8090@users.noreply.github.com>
2020-04-19 22:36:35 +0200

    Update contact_page.dart

--- lib/pages/contact_page.dart
+++ lib/pages/contact_page.dart
@@ -10,8 +10,70 @@ class ContactPage extends StatelessWidget {
     return Scaffold(
       appBar: Header(),
       drawer: getAppDrawer(context),
-      body: Center(
-        child: Text('Kontakt'),
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(50),
+            child: Center(
+                child: Text(
+              'Kontakt',
+              style: Theme.of(context).textTheme.headline2,
+            )),
+          ),
+          Row(
+            children: <Widget>[
+              Flexible(
+                fit: FlexFit.tight,
+                flex: 15,
+                child: Text('Telefon: 123-456-789'),
+              ),
+              Flexible(
+                fit: FlexFit.tight,
+                flex: 10,
+                child: Column(
+                  children: <Widget>[
+                    buildListTile(
+                      context,
+                      title: '+48 123 456 789',
+                      icon: Icons.phone,
+                    ),
+                    buildListTile(
+                      context,
+                      title: 'biuro@nieruchomosci-waw.com',
+                      icon: Icons.email,
+                    ),
+                    buildListTile(
+                      context,
+                      title: 'Numer konta: 12 1234 1234 1234 1234 1234 1234',
+                      icon: Icons.attach_money,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildListTile(BuildContext context, {String title, IconData icon}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 5,
+        horizontal: 20,
+      ),
+      child: Card(
+        child: ListTile(
+          leading: Icon(
+            icon,
+            size: 35,
+          ),
+          title: Text(
+            title,
+            style: Theme.of(context).textTheme.headline5,
+          ),
+        ),
       ),
     );
   }
